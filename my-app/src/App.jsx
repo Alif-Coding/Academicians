8de74919926872df32321752a185fc5afb7dd08c
Alif Kenzie <alifkenzie77@gmail.com>
2025-07-09 15:46:59 +0700

    update pertama

---
+++ my-app/src/App.jsx
@@ -0,0 +1,36 @@
+import { useEffect, useState } from "react";
+import { createClient } from "@supabase/supabase-js";
+
+import "./App.css";
+
+const supabase = createClient(
+  import.meta.env.VITE_SUPABASE_URL,
+  import.meta.env.VITE_SUPABASE_ANON_KEY
+);
+
+function App() {
+  const [instruments, setInstruments] = useState([]);
+  useEffect(() => {
+    getInstruments();
+  }, []);
+  async function getInstruments() {
+    const { data, error } = await supabase.from("instruments").select();
+    if (error) {
+      console.error("Supabase error:", error.message);
+      return;
+    }
+    setInstruments(data || []);
+  }
+  return (
+    <>
+      <h1>hello</h1>
+      <ul>
+        {instruments.map((instrument) => (
+          <li key={instrument.name}>{instrument.name}</li>
+        ))}
+      </ul>
+    </>
+  );
+}
+
+export default App;
