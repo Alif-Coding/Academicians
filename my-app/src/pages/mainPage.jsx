3bfe9b81ae9d8bfd55a8e8e279085a29e95e17c7
PapermasteroneGH <papermasteroney@gmail.com>
2025-07-12 12:13:57 +0700

    update

--- my-app/src/pages/mainPage.jsx
+++ my-app/src/pages/mainPage.jsx
@@ -1,12 +1,19 @@
+import Split from 'react-split';
 
 export default function Mainpage() {
     
-    
-    
     return (
         <>
-            <div class="mainPage">
-            
+            <div class="sidebar">
+                
             </div>
+            <Split class="mainPage" split="vertical">
+                <div class="hallWay">
+
+                </div>
+                <div class="classRooms">
+
+                </div>
+            </Split>
         </>)
 }
