f1c869826809a00204b68b609f65f6124a308c84
PapermasteroneGH <papermasteroney@gmail.com>
2025-07-14 20:58:21 +0700

    update

--- my-app/src/pages/landingPage.jsx
+++ my-app/src/pages/landingPage.jsx
@@ -6,6 +6,7 @@ function Landingpage() {
             <div class="">
                 <div class="section-1 bg-slate-950 h-full w-full">
                     
+
                 </div>
                 <div class="section-2">
 
