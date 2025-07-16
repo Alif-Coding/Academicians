a2e4c5838c60124fab2197cdf60b88e47f61f679
PapermasteroneGH <papermasteroney@gmail.com>
2025-07-16 09:08:43 +0700

    update

--- my-app/src/components/navbar.jsx
+++ my-app/src/components/navbar.jsx
@@ -3,19 +3,19 @@ import { Link } from 'react-router-dom';
 export default function Navbar() {
 
     return(<>
-        <div className="h-16 w-full text-blue-600 flex items-center justify-between px-4 absolute ">
+        <div className="h-16 w-full text-blue-600 flex items-center px-[80px] absolute ">
             <Link to="/">
-                <h1 class="hover:text-blue-200">
+                <h1 class="hover:text-blue-200 px-[80px]">
                     Landing Page
                 </h1>
             </Link>
             <Link to="/Pricing">
-                <h1 class="hover:text-blue-200">
+                <h1 class="hover:text-blue-200 px-[80px]">
                     Pricing
                 </h1>
             </Link>
             <Link to="/Company">
-                <h1 class="hover:text-blue-200">
+                <h1 class="hover:text-blue-200 px-[80px]">
                     Company
                 </h1>
             </Link>
