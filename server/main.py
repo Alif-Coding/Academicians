c4cd37b85f5a584cb113bf7e7394eb52604789fb
PapermasteroneGH <papermasteroney@gmail.com>
2025-07-19 14:00:06 +0700

    initiate main.py and server

---
+++ server/main.py
@@ -0,0 +1,21 @@
+from fastapi import FastAPI, Request
+from fastapi.middleware.cors import CORSMiddleware
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["https://localhost:5173"],
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+@app.get("/")
+def read_root():
+    return {"message": "skibidi pytong"}
+
+@app.post("/process")
+async def process_Data(request: Request):
+    body = await request.json()
+    return {"result": body}
+    
